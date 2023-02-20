420610df4c9d63693db2c5670b48fbb7cc6c6d24
liza <123323177+younggengaar@users.noreply.github.com>
2023-02-20 22:56:24 +0300

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание1
+            //Console.Write("Введите размерность массива:");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] omas = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadKey();
+
+            //задание2
+            //int[] omas = new int[] { 1, 2, 3, 4, 5 };
+            //for (int i=0; i<omas.Length; i++)
+            //{
+            // Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadKey();
+
+            //задание3
+            //int [] A= new int[10];
+            //for( int i=0; i<10; i++)
+            //{
+            // A[i]=i;
+            //}
+            //Console.WriteLine("Элементы массива:");
+            //foreach (int i in A)
+            //{
+            // Console.WriteLine("A[{0}]={1}", i, A[i]);
+            //}
+            //Console.ReadKey();
+
+            //задание4
+            //int[] omas = new int[5];
+            //Random rnd = new Random();
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] = rnd.Next();
+            //}
+            //foreach (int i in omas)
+            //{
+            //    Console.WriteLine($"omas[{i}]={i}");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
